6179e1a800f193ca6bbf82f6cdfc0a3df09ae75d
Piotr Drajewicz <drpiterek@gmail.com>
2022-06-23 01:13:46 +0200

    navbar is working, now will be adding switching pages

--- public/radar.js
+++ public/radar.js
@@ -10,6 +10,25 @@ window.addEventListener('load', () => { //run function when page is loaded
 	const boundariesPoints = [];
 	const workButton = document.getElementById('work-btn');
 
+	//navlinks
+	const tableLink = document.querySelector('.table-link');
+	const filtersLink = document.querySelector('.filters-link');
+
+	filtersLink.addEventListener('click', () => {
+		//show filters window
+		if (!filtersLink.classList.contains('nav-link-active')) {
+			filtersLink.classList.add('nav-link-active');
+			tableLink.classList.remove('nav-link-active');
+		}
+	});
+	tableLink.addEventListener('click', () => {
+		//show table window
+		if (!tableLink.classList.contains('nav-link-active')) {
+			tableLink.classList.add('nav-link-active');
+			filtersLink.classList.remove('nav-link-active');
+		}
+	});
+
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(position => {
 			lat = position.coords.latitude;
