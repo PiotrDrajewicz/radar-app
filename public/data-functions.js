fc53c2fb978a8c949de929434ab77b097fee9047
Piotr Drajewicz <drpiterek@gmail.com>
2022-08-15 15:39:54 +0200

    app now working when there is no plane in watched area, table rows color is back as it was - I need to fix responsiveness primarly

--- public/data-functions.js
+++ public/data-functions.js
@@ -1,35 +1,38 @@
 export { fetchData, extractData };
 
 function fetchData(api) {
-    return new Promise((resolve, reject) => {
-        console.log('fetching data');
-        resolve(fetch(api)
-            .then(response => {
-                return response.json();
-            })
-            // .then(data => {
-            // 	// const allPlanes = [];
-            // 	data.states.forEach(plane => {
-            // 		allPlanes.push(plane);
-            // 	})
-            // 	console.log('api done');
-            // 	// console.log(allPlanes);
-            // 	return allPlanes;
-            // })
-        )
-        // reject(console.log('nie udało się'));
-    })
+  return new Promise((resolve, reject) => {
+    console.log("fetching data");
+    resolve(
+      fetch(api).then((response) => {
+        return response.json();
+      })
+      // .then(data => {
+      // 	// const allPlanes = [];
+      // 	data.states.forEach(plane => {
+      // 		allPlanes.push(plane);
+      // 	})
+      // 	console.log('api done');
+      // 	// console.log(allPlanes);
+      // 	return allPlanes;
+      // })
+    );
+    // reject(console.log('nie udało się'));
+  });
 }
 
 function extractData(data) {
-    const allPlanes = [];
-    return new Promise(resolve => {
-        console.log('extracting data');
+  const allPlanes = [];
+  return new Promise((resolve) => {
+    console.log("extracting data");
 
-        data.states.forEach(plane => {
-            allPlanes.push(plane);
-        })
-
-        resolve(allPlanes);
-    })
+    try {
+      data.states.forEach((plane) => {
+        allPlanes.push(plane);
+      });
+      resolve(allPlanes);
+    } catch (error) {
+      console.log("no planes in watched area");
+    }
+  });
 }
